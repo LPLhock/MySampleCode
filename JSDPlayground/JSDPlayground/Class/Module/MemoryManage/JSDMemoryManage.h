d15aece03ba08d22634bbec09a945fa8df97c2ae
Jersey <zexi0625@gmail.com>
2019-12-25 20:47:54 +0800

    新增内存管理模块

---
+++ JSDPlayground/JSDPlayground/Class/Module/MemoryManage/JSDMemoryManage.h
@@ -0,0 +1,17 @@
+//
+//  JSDMemoryManage.h
+//  JSDPlayground
+//
+//  Created by Jersey on 25/12/2019.
+//  Copyright © 2019 Jersey. All rights reserved.
+//
+
+#import "JSDViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSDMemoryManage : JSDViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
